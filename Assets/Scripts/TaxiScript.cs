4dcf62203e60c35ee0e07b35e063f2717b5b7ba9
morpheus440 <pincurow@sheridancollege.ca>
2026-05-29 15:42:04 -0400

    Did stuff

---
+++ Assets/Scripts/TaxiScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TaxiScript : MonoBehaviour
+{
+    private Vector2 directionalInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+    }
+}
